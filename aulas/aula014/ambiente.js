c8ea44a33c864f66f726403e7d09f718e71ee2eb
Carlos Alexandre <138731346+carlosalexdev@users.noreply.github.com>
2023-07-06 16:07:51 -0300

    Initial commit

---
+++ aulas/aula014/ambiente.js
@@ -0,0 +1,7 @@
+console.log('Vai começar...')
+
+for (var c = 1; c <= 4; c++) {
+    console.log(`Passo ${c}`)
+}
+
+console.log('FIM')
---
+++ exercicios/ex001/script.js
@@ -0,0 +1,17 @@
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12){
+        img.src = 'manha.png'
+        document.body.style.background ='#fedeae'
+    }else if (hora>= 12 && hora <= 18){
+        img.src = 'tarde.png'
+        document.body.style.background = '#94a1b4'
+    } else {
+        img.src = 'noite.png'
+        document.body.style.background = '#121f3e'
+    }
+}
